9142bf7e64d08fae23565046eba1d31cb8c10769
Robert Hoang <robert.hoang@theaccessgroup.com>
2024-05-10 15:53:11 +0700

    commit for TC04 - default dropdown

--- SeleniumTesting/Webdriver/DropdownList_Default.java
+++ SeleniumTesting/Webdriver/DropdownList_Default.java
@@ -1,26 +1,33 @@
 package Webdriver;
+
 import com.sun.source.tree.ImportTree;
-import org.openqa.selenium.By ;
-import org.openqa.selenium.WebElement ;
-import org.openqa.selenium.WebDriver ;
-import org.openqa.selenium.chrome.ChromeDriver ;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import org.openqa.selenium.support.ui.WebDriverWait ;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
+
 import AutomationFCUtils.CommonUtils;
 import org.testng.Assert;
+import org.openqa.selenium.support.ui.Select;
+
 public class DropdownList_Default {
     WebDriver driver;
     WebDriverWait wait;
     CommonUtils commonUtils = new CommonUtils();
 
+    Select select;
 
- @BeforeClass
+    @BeforeClass
     public void initPage() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://demo.nopcommerce.com/register");
     }
+
     @Test(priority = 1, description = "Register form")
     public void registerForm() {
         // Step 01: Click Register link
@@ -45,15 +52,22 @@ public class DropdownList_Default {
 
         commonUtils.clickElement(driver.findElement(By.xpath("//button[@id=\"register-button\"]")));
     }
-    @Test(priority = 2,description = "Verify register success")
+
+    @Test(priority = 2, description = "Verify register success")
     public void verifyRegisterSuccess() {
+        // Step 01: Click on My account button
         WebElement btnmyaccount = driver.findElement(By.xpath("//a[@class=\"ico-account\"]"));
         commonUtils.clickElement(btnmyaccount);
+        // Step 02: Verify register success with DATE OF BIRTH
+        select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "1");
+        //Step 03: Verify register success with Month OF BIRTH
+        select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "October");
 
-        Assert.assertEquals(driver.findElement(By.xpath("//select[@name=\"DateOfBirthDay\"]")).getAttribute("value"), "1");
-        Assert.assertEquals(driver.findElement(By.xpath("//select[@name=\"DateOfBirthMonth\"]")).getAttribute("value"), "October");
-        Assert.assertEquals(driver.findElement(By.xpath("//select[@name=\"DateOfBirthYear\"]")).getAttribute("value"), "1998");
-
+        //Step 03: Verify register success with Year OF BIRTH
+        select = new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthYear']")));
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "1998");
 
     }
 }
